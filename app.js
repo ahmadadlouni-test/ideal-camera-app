bda533b506539f6a3a07e96e51793f2979e55f05
ahmadadlouni-test <88533670+ahmadadlouni-test@users.noreply.github.com>
2021-09-29 12:22:58 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -14,18 +14,20 @@
 
 // const constraints = { 
 //     video: { 
+//        resizeMode: 'crop-and-scale',
 //         facingMode: 'environment', 
-//         width: { min: 600, max: 1000 }, 
-//         height: { min: 378, max: 630 } 
+//         width: { min: 600, ideal: 1000 }, 
+//         height: { min: 378, ideal: 630 } 
 //     }, 
 //     audio: false 
 // };
 
-// const constraints = { 
+// const secondConstraints = { 
 //     video: { 
+//         resizeMode: 'crop-and-scale',
 //         facingMode: 'environment', 
-//         width: { min: 378, ideal: 630, max: 630 }, 
-//         height: { min: 600, ideal: 1000, max: 1000 } 
+//         width: { min: 378, ideal: 630 }, 
+//         height: { min: 600, ideal: 1000 } 
 //     }, 
 //     audio: false 
 // };
@@ -42,15 +44,26 @@ const constraints = {
     audio: false 
 };
 
-const secondConstraints = {
+const secondConstraints = { 
     video: { 
         resizeMode: 'crop-and-scale',
         facingMode: { exact: 'environment' }, 
-        width: { min: 600, ideal: 1000 }, 
-        height: { min: 378, ideal: 630 } 
+        width: { min: 378, max: 630 }, 
+        height: { min: 600, max: 1000 } 
     }, 
     audio: false 
 };
+
+
+// const secondConstraints = {
+//     video: { 
+//         resizeMode: 'crop-and-scale',
+//         facingMode: { exact: 'environment' }, 
+//         width: { min: 600, ideal: 1000 }, 
+//         height: { min: 378, ideal: 630 } 
+//     }, 
+//     audio: false 
+// };
 
 // const constraints = { 
 //     video: { 
